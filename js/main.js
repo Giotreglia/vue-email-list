a025babd2721d2d95661c4a1dd687bd1308a2739
Giotreglia <giovanni.treglia@gmail.com>
2023-03-23 14:10:50 +0100

    altre modifiche

--- js/main.js
+++ js/main.js
@@ -7,16 +7,16 @@ createApp({
     }
   },
   mounted() {
-
     
     for (let i = 0; i < 10; i++) {
       axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-      
+
       .then( (response) => {
-              this.mailArray.push(response.data.response);
+            this.mailArray.push(response.data.response);
           }
       )
     }
-
+    console.log(this.mailArray);
+    
   }
 }).mount('#app')
