6b39b0ee0c7502fe2a5fd4e206ab28f496b54a1c
Giotreglia <giovanni.treglia@gmail.com>
2023-03-23 13:49:48 +0100

    Lista mail creata attraverso utilizzo API

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      mailArray: []
+    }
+  },
+  mounted() {
+
+    
+    for (let i = 0; i < 10; i++) {
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      
+      .then( (response) => {
+              this.mailArray.push(response.data.response);
+          }
+      )
+    }
+
+  }
+}).mount('#app')
